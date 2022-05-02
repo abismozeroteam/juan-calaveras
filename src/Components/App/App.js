c173c96ff4fefc9aa5cca055bd2e16cb1eee5e64
eduairet <hola@eduairet.com>
2022-05-02 18:21:26 -0500

    Platillos

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import logo from '../Util/images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>JUAN CALAVERAS</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
